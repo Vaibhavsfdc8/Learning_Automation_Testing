7bca06b7b851118e4669b816cb3c9cb2c1950a7f
VaibhavGhadge <vaibhav.sfdc8@gmail.com>
2024-07-31 15:44:29 +0530

    New Lab002 has added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Programs
+- Lab001
+- Lab002
---
+++ src/EX_31072024/Lab002.java
@@ -0,0 +1,7 @@
+package EX_31072024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Hello Vaibhav");
+    }
+}
