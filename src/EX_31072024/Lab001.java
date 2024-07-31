8efb73c6a3f2f019d7f77c1d9a5dc87d217c5b53
VaibhavGhadge <vaibhav.sfdc8@gmail.com>
2024-07-31 16:18:51 +0530

    New Lab001 changes been has added

--- src/EX_31072024/Lab001.java
+++ src/EX_31072024/Lab001.java
@@ -1,8 +1,19 @@
 package EX_31072024;
+import java.util.Scanner;
 
 public class Lab001 {
+    public void Test(){
+        System.out.println("Green");
+    }
     public static void main(String[] args) {
 
         System.out.println("MY First Program");
+        Lab001 l1= new Lab001();
+        l1.Test();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();
+        System.out.println("Username is: " + userName);
     }
 }
